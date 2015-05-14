32ff5d79010add41e6944ce124faef3b9fa3b0b7
leonardlee2015 <leonardlee2013@gmail.com>
2015-05-15 00:01:21 +0800

    Initial Commit

---
+++ LeonPhotomania/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeonPhotomania
+//
+//  Created by 李南 on 15/5/15.
+//  Copyright (c) 2015年 ctd.leonard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
